65ab76c835d64b4f2d85dd28dc405fe6f2b127c6
Chandana Sapparapu <chandana@Chandanas-Mini.home>
2015-06-06 00:01:33 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,38 @@
+import sbtassembly.{PathList, MergeStrategy}
+
+name := "HelloScala"
+
+version := "1.0"
+
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+{
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+}
+
+scalaVersion := "2.10.4"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-streaming" % "1.3.1" % "provided",
+  "org.apache.spark" %% "spark-core" % "1.3.1" % "provided",
+  "org.apache.spark" %% "spark-sql" % "1.3.1" % "provided",
+  "org.apache.spark" %% "spark-hive" % "1.3.1" % "provided",
+  "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.3.0")
+
+
+
+resolvers ++= Seq(
+  "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/",
+  "Spray Repository" at "http://repo.spray.cc/",
+  "Cloudera Repository" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
+  "Akka Repository" at "http://repo.akka.io/releases/",
+  "Twitter4J Repository" at "http://twitter4j.org/maven2/",
+  "Apache HBase" at "https://repository.apache.org/content/repositories/releases",
+  "Twitter Maven Repo" at "http://maven.twttr.com/",
+  "scala-tools" at "https://oss.sonatype.org/content/groups/scala-tools",
+  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+  "Second Typesafe repo" at "http://repo.typesafe.com/typesafe/maven-releases/",
+  "Mesosphere Public Repository" at "http://downloads.mesosphere.io/maven",
+  Resolver.sonatypeRepo("public")
+)
---
+++ src/main/scala/TwitterPopularTags.scala
@@ -0,0 +1,63 @@
+
+/**
+ * Created by chandana on 6/4/15.
+ */
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+//import org.apache.spark.streaming.twitter.TwitterUtils
+
+object TwitterPopularTags {
+  def main(args: Array[String]) {
+    //    if (args.length < 4) {
+    //      System.err.println("Usage: TwitterPopularTags <consumer key> <consumer secret> " +
+    //        "<access token> <access token secret> [<filters>]")
+    //      System.exit(1)
+    //    }
+
+//    StreamingExamples.setStreamingLogLevels()
+
+    //    val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
+    val filters = args.takeRight(args.length)
+
+    // Set the system properties so that Twitter4j library used by twitter stream
+    // can use them to generat OAuth credentials
+    System.setProperty("twitter4j.oauth.consumerKey", "G4ua2YYHQOZ1xFw8WxgZiEmPX")
+    System.setProperty("twitter4j.oauth.consumerSecret", "mIDCx8E55LVWNe6jVbwz7MuaNDPGhxrBThgzeCbEQvT6yNUdTn")
+    System.setProperty("twitter4j.oauth.accessToken", "13355462-PURXJJfJFk8c0pr9S5nIlkV4yqAiv5ay0UyiD5IXN")
+    System.setProperty("twitter4j.oauth.accessTokenSecret", "UJ4emncHNwlFvXSTTt31fk59R5WrTIHCycFqGWqZ4YSyu")
+
+    val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val stream = TwitterUtils.createStream(ssc, None, filters)
+
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map{case (topic, count) => (count, topic)}
+      .transform(_.sortByKey(false))
+
+    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map{case (topic, count) => (count, topic)}
+      .transform(_.sortByKey(false))
+
+
+    // Print popular hashtags
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    })
+
+    topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
